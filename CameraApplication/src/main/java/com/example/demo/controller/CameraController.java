1db1f48444e2815ddda381506bd79cda8911ebc6
dhsong <dhsong@nextree.co.kr>
2018-03-14 14:45:11 +0900

    프로젝트 추가

---
+++ CameraApplication/src/main/java/com/example/demo/controller/CameraController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class CameraController {
+
+	@Value("${market.type}")
+	private String marketType;
+	
+	@Value("${db.server.ip}")
+	private String dbServer;
+	
+	@Value("${user}")
+	private String user;
+	
+	@RequestMapping(value="/camera/viewconfiguration", method=RequestMethod.GET)
+	public String viewConfiguration() {
+		System.out.println(marketType);
+		System.out.println(dbServer);
+		System.out.println(user);
+		return "aaaaa";
+	}
+}
